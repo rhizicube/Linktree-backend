4f320204eb1fe1e0a740b86eb73bb6b17d36dd91
Mansi Garg <56781938+mansigarg-nith@users.noreply.github.com>
2023-02-02 22:37:26 +0530

    Modified analytics

--- router/analysis.py
+++ router/analysis.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import session
-from schemas.clicks_resample import RequestClick, ResponseClick, UpdateClick
 import crud.clicks_resample as clicks_resample
 import crud.views_resample as views_resample
 from crud.profiles import get_profile_by_id
 from db_connect.setup import get_db
+from db_connect.config import postgre_engine
 from datetime import datetime as dt
-import datetime
+from sqlalchemy import inspect
 import json
 import pandas as pd
 from fastapi.encoders import jsonable_encoder
@@ -195,83 +195,58 @@ analysis_router = APIRouter()
 #                 return JSONResponse(code=status.HTTP_404_NOT_FOUND, status="Not Found", result={}, message="Profile not found")
 #     except:
 #         return JSONResponse(content={"message": f"Profile {id} not found"}, status_code=status.HTTP_404_NOT_FOUND)
-
-
 ## return count of  views and clicks using pandas
 @analysis_router.get("/analysis/")
-async def get(id: int, start: dt = None, end: dt = None, db: session = Depends(get_db)):
+async def get(id: int, start: dt, end: dt, db: session = Depends(get_db)):
     try:
-        _profile = get_profile_by_id(db=db, id=id)
-        _views = views_resample.get_views_by_profile_id(db, id)
-        views_count = []
-        views_created = []
-        for _view in _views:
-            views_count.append(_view.view_count)
-            views_created.append(_view.view_created.date())
-        views_json = jsonable_encoder(_views)
-        _views_df = pd.DataFrame(views_json)
+        with postgre_engine.connect() as conn:
+            query = f"SELECT * FROM ViewsResample WHERE profile_id = {id} AND view_sampled_timestamp BETWEEN '{start}' AND '{end}'"
+            views_table = conn.execute(query)
+        _views_df = pd.DataFrame(views_table.fetchall())
+        _views_df.columns = views_table.keys()
+        view_ids = []
+        for view in _views_df["id"]:
+            view_ids.append(view)
         _views_df["view_sampled_timestamp"] = pd.to_datetime(_views_df["view_sampled_timestamp"])
         _views_table = pd.pivot_table(_views_df, values='view_count', index=['view_sampled_timestamp'], columns=['session_id']).fillna(0)
-        # print(_views_table)
         views_table = _views_table.resample('D').sum()
         views_table = views_table.reset_index()
         views_table["view_sampled_timestamp"] = views_table["view_sampled_timestamp"].dt.strftime('%Y-%m-%d')
         views_response_data = json.loads(views_table.to_json(orient='records'))
         # Use dict_variable.items() to iterate through the dictionary
         for i in range(len(views_response_data)):
-            for j in views_response_data[i]:
-                if j != "view_sampled_timestamp":
-                    views_response_data[i][j] = {"session_id": j, "view_count": views_response_data[i][j]}
+            for k,v in views_response_data[i].items():
+                if k!= "view_sampled_timestamp":
+                    views_response_data[i][k] = {"session_id": k, "view_count": v}
         _clicks = []
-        for _view in _views:
-            _click = clicks_resample.get_click_by_view_id(db=db, view_id=_view.id)
-            for c in _click:
-                _clicks.append(c)
-        _clicks_df = pd.DataFrame(jsonable_encoder(_clicks[0]), index=[0])
-        for c in _clicks[1:]:
-            _clicks_df.loc[len(_clicks_df)] = jsonable_encoder(c)
-        print(_clicks_df)
+        _clicks_df = pd.DataFrame()
+        with postgre_engine.connect() as conn:
+            query = f"SELECT * FROM ClicksResample WHERE view_id = {view_ids[0]} AND click_sampled_timestamp BETWEEN '{start}' AND '{end}'"
+            clicks_table = conn.execute(query)
+        _clicks_df = pd.DataFrame(clicks_table.fetchall())
+        _clicks_df.columns = clicks_table.keys()
+        for view_id in view_ids[1:]:
+            with postgre_engine.connect() as conn:
+                query = f"SELECT * FROM ClicksResample WHERE view_id = {view_id} AND click_sampled_timestamp BETWEEN '{start}' AND '{end}'"
+                clicks_table = conn.execute(query)
+            _click_df = pd.DataFrame(clicks_table.fetchall())
+            _click_df.columns = clicks_table.keys()
+            _clicks_df = _clicks_df.append(_click_df)
         _clicks_df["click_sampled_timestamp"] = pd.to_datetime(_clicks_df["click_sampled_timestamp"])
         _clicks_table = pd.pivot_table(_clicks_df, values='click_count', index=['click_sampled_timestamp'], columns=['view_id']).fillna(0)
-        print(_clicks_table)
         clicks_table = _clicks_table.resample('D').sum()
         clicks_table = clicks_table.reset_index()
         clicks_table["click_sampled_timestamp"] = clicks_table["click_sampled_timestamp"].dt.strftime('%Y-%m-%d')
         clicks_response_data = json.loads(clicks_table.to_json(orient='records'))
         # Use dict_variable.items() to iterate through the dictionary
         for i in range(len(clicks_response_data)):
-            for j in clicks_response_data[i]:
-                if j != "click_sampled_timestamp":
-                    clicks_response_data[i][j] = {"view_id": j, "click_count": clicks_response_data[i][j]}
+            for k,v in clicks_response_data[i].items():
+                if k!= "click_sampled_timestamp":
+                    clicks_response_data[i][k] = {"view_id": k, "click_count": v}
         if start != None and end != None:
             # return views_response_data and clicks_response_data between start and end
             start = start.strftime('%Y-%m-%d')
             end = end.strftime('%Y-%m-%d')
-            views_response_data = [i for i in views_response_data if i["view_sampled_timestamp"] >= start and i["view_sampled_timestamp"] <= end]
-            clicks_response_data = [i for i in clicks_response_data if i["click_sampled_timestamp"] >= start and i["click_sampled_timestamp"] <= end]
             return JSONResponse(content={"message": {"views": views_response_data, "clicks": clicks_response_data}}, status_code=status.HTTP_200_OK)
     except:
-        print("here")
         return JSONResponse(content={"message": f"Profile {id} not found"}, status_code=status.HTTP_404_NOT_FOUND)
-
-# Comments:
-#     1. The above code will query everything in the DB, and then pick only those rows that is between the given date range. This is inefficient. Query the DB only for rows between the given date range, dont query unnecessary data 
-#     2. Use inbuilt dict functions to iterate over dictionaries, running manual loops arent required. (look up dict_var.items())
-#     3. Add doc string 
-#     4. Add comments wherever issues were faced, so its not forgotten again
-#     5. can use raw sql queries for complex queries. Add dictionaries for parameters to prevent SQL injection
-#     6. Try keeping functions as short as possible
-
-
-
-
-
-
-# "28-01-2023":{
-#     "session_id": "kdhjkd",
-#     "view_count": 1,
-#     "view_sampled_timestamp": "2023-01-28 00:00:00"
-# },
-# "29-01-2023":{
-#     "session_id": "kdhjkd",
-# }
